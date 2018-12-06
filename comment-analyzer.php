f685c9b6f3b500870a2b330b35d406506a572b35
shailesh12 <shailesh.tyagi@live.com>
2018-12-06 16:12:58 +0530

    fixes & enahcements

--- comment-analyzer.php
+++ comment-analyzer.php
@@ -7,40 +7,30 @@
  * Author: Shailesh Tyagi
  * Version: 0.2
  */
-class CommentAnalysis {
+class comment_analysis {
 
     public function __construct() {
-        $this->includeFiles();
-        add_filter('manage_edit-comments_columns', array($this, 'addSentimentColumn'), 10, 1);
-        add_action('manage_comments_custom_column', array($this, 'showSentimentColumnResponse'), 10, 2);
-        add_action('comment_post', array($this, 'saveSentimentResponse'), 10, 1);
-    }
-
-    /**
-     * Function to include external files into this plugin
-     */
-    function includeFiles() {
-        require_once(ABSPATH . "wp-admin/includes/upgrade.php");
-        require_once(ABSPATH . "wp-includes/comment-template.php");
-        require_once(ABSPATH . "wp-content/plugins/comment-analyzer/php-sentiment-analyser/lib/sentiment_analyser.class.php");
+        add_filter('manage_edit-comments_columns', array($this, 'add_sentiment_coloumn'), 10, 1);
+        add_action('manage_comments_custom_column', array($this, 'show_sentiment_column'), 10, 2);
+        add_action('comment_post', array($this, 'save_sentiment_response'), 10, 1);
     }
 
     /**
      * Add sentiment type column on manage comment page(admin)
      * @param array $cols
-     * @return type
+     * @return array
      */
-    function addSentimentColumn($cols) {
+    function add_sentiment_coloumn($cols) {
         $cols['api_result'] = __('Sentiment Type', 'wpse64973');
         return $cols;
     }
 
     /**
      * Display sentiment type field value on manage comment page(admin)
-     * @param type $col
-     * @param type $comment_id
+     * @param string $col
+     * @param int $comment_id
      */
-    function showSentimentColumnResponse($col, $comment_id) {
+    function show_sentiment_column($col, $comment_id) {
         if ($col == 'api_result') {
             if ($t = get_comment_meta($comment_id, 'api_sentiment_response', true)) {
                 echo esc_html($t);
@@ -52,12 +42,12 @@ class CommentAnalysis {
 
     /**
      * Save sentiment response into database
-     * @param type $comment_ID
+     * @param int $comment_ID
      */
-    function saveSentimentResponse($comment_ID) {
-        //function logic goes here
+    function save_sentiment_response($comment_ID) {
         $comment_text = get_comment_text($comment_ID);
         // Analyze comment sentiment using open source third party libaray
+        require_once(ABSPATH . "wp-content/plugins/comment-analyzer/php-sentiment-analyser/lib/sentiment_analyser.class.php");
         $sa = new SentimentAnalysis();
         $sa->initialize();
         $sa->analyse($comment_text);
@@ -83,5 +73,6 @@ class CommentAnalysis {
 /**
  * Create instance of the class to kick off the whole things
  */
-new CommentAnalysis();
+new comment_analysis();
+
 
